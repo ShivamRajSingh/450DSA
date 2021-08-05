64088aa25898e242055c105d9f92b5504ac1729d
Shivam Raj Singh <48837530+shivam3009@users.noreply.github.com>
2021-08-05 18:25:25 +0530

    5-8-21

---
+++ Linked List/Merge_sort_in_linked_lists.cpp
@@ -0,0 +1,14 @@
+#include "codeblock.h"
+
+Node* mergeSort(Node *l1, Node *l2){
+
+}
+
+int main()
+{
+    Node* l1 = input();
+    Node* l2 = input();
+    Node* l3 = mergeSort(l1,l2);
+    print(l3);
+    return 0;
+}
